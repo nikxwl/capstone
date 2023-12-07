7db6fa87032aca3b7c267e633b08fd871fa24288
Kate Honey Rose Manalo <keytmanalo.11@gmail.com>
2023-12-08 05:05:33 +0800

    student page

---
+++ v3/Student.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<html>
+    <head> 
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+		
+		<link rel="stylesheet" href="">
+	 	<title> Student - Graduate Tracer </title>	
+    </head>
+    
+    <body>
+        <nav class="navbar bg-primary-subtle">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              <img src="Pictures/uphsl%20logo.png" alt="logo" width="75" height="100">
+            </a>
+              
+            <h3><strong><em>UPHSL Graduate Tracer</em></strong></h3>
+              <a href="Main.php">
+            <button type="button" class="btn btn-secondary ">Logout</button>
+            </a>
+          </div>
+        </nav>
+    
+        <
+        
+        
+        
+        
+        
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </body>
+</html>
